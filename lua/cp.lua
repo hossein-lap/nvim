6f7120caeafdef6811f1318a321e7c9450fe4370
hossein <hosteam01@gmail.com>
2022-07-06 04:03:03 +0430

    wrote compile/run function
    - `make' command finished.
    
    TODO: - compile/run command

--- lua/cp.lua
+++ lua/cp.lua
@@ -4,23 +4,35 @@
 -- |_||_|___|   P
 --
 
-function TermWrapRun(wrapandrun)
+function TermWrapRun(wrapandrun, file_name)
 	if not split_style then
-		split_style = 'v'
+		split_style = 'h'
 	end
 
 	if split_style == 'v' then
-		buffercmd = '50vnew'
+		buffercmd = '45vnew'
 	elseif split_style == 'h' then
 		buffercmd = '15new'
 	else
-		print("ERROR! split_style is not a valid value (must be 'h' or 'v')\n")
+		print("ERROR!")
+		print("split_style have not a valid value")
+		print("(must be 'h' or 'v')")
 	end
 
-	print(wrapandrun)
-
+	--print(wrapandrun)
+	api.nvim_command(buffercmd)
+	api.nvim_command('setlocal nornu nonu')
+	if file_name then
+		api.nvim_command('term ' .. wrapandrun .. ' ' .. file_name)
+	else
+		api.nvim_command('term ' .. wrapandrun)
+	end
+	api.nvim_command('startinsert')
 end
 
-split_style = 'v'
+split_style = 'h'
 
-TermWrapRun("This is test")
+--fpath = api.nvim_buf_get_name(0)
+--map('n', '<leader>cc', ':lua TermWrapRun("make ", fpath)<CR>')
+
+map('n', '<leader>cc', ':lua TermWrapRun("make")<CR>')
