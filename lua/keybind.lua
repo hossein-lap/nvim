cbdf6f9627ff5589ec3f8f566978e82b25565a9e
hossein <hosteam01@gmail.com>
2022-07-07 03:10:49 +0430

    made spell check command silent
    change some string styles' (wrote them with [[ ]] )

--- lua/keybind.lua
+++ lua/keybind.lua
@@ -5,24 +5,32 @@ map('n', '<leader>ff', ':24Lexplore<CR><C-w>w')
 map('n', '<C-P>', ':set paste!<CR>')
 
 -- spelling check
-map('n', '<leader>ss',  ':setlocal spell!<CR>')
+map('n', '<leader>ss',  ':setlocal spell!<CR>', {silent = true})
+
+-- clear search highlight
+map('n', '<C-n>',  ':set nohlsearch<CR>', {silent = true})
 
 -- switch tabs
 map('n', '_', ':tabprevious<CR>', {silent = true})
 
 -- comment lines via - and uncomment via +
 au({ 'c', 'cpp', 'java', 'scala', 'json' },
-	"let b:comment_leader = '// '")
+	[[let b:comment_leader = '//']])
 au({ 'sh', 'ruby', 'python', 'conf', 'yaml', 'make', 'toml', 'rmd' },
-	"let b:comment_leader = '# '")
+	[[let b:comment_leader = '#']])
 au({ 'haskell', 'lua', },
-	"let b:comment_leader = '-- '")
+	[[let b:comment_leader = '--']])
 au({ 'vim', },
-	[[let b:comment_leader = '" ']])
+	[[let b:comment_leader = '"']])
 au({ 'tex', 'plaintex', },
-	"let b:comment_leader = '% '")
+	[[let b:comment_leader = '%']])
 au({ 'nroff', },
-	[[let b:comment_leader = '\" ']])
+	[[let b:comment_leader = '\"']])
+
+au({ 'asm', },
+	[[let b:comment_leader = ';']])
+
+-- cmd 'let'
 
 -- map the keybinding
 map('',
@@ -31,14 +39,14 @@ map('',
 	{ silent = true }
 )
 
-map('',
-	'+',
-	[[:s;\v^(\s*)\<C-R>=escape(b:comment_leader,'\; ')<CR>;\1;<CR>:nohlsearch<CR>]],
-	{ silent = true }
-)
+-- map('',
+	-- '+',
+	-- [[:s;\v^(\s*)\<C-R>=escape(b:comment_leader,'\; ')<CR>;\1;<CR>:nohlsearch<CR>]],
+	-- { silent = true }
+-- )
 
 
 cmd [[
 "noremap <silent> - :<C-B>silent <C-E>s;\v^(\s*);\1<C-R>=escape(b:comment_leader,'\ ')<CR><CR>:nohlsearch<CR>
-"noremap <silent> + :<C-B>silent <C-E>s/\v^(\s*)\<C-R>=escape(b:comment_leader,'\/ ')<CR>/\1/<CR>:nohlsearch<CR>
+noremap <silent> + :<C-B>silent <C-E>s;\v^(\s*)\<C-R>=escape(b:comment_leader,'\; ')<CR>;\1;<CR>:nohlsearch<CR>
 ]]
