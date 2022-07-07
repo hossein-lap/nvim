7e5cf819c1850ec3b8016437bb8ca7a8a7f2873d
hossein <hosteam01@gmail.com>
2022-07-07 23:01:19 +0430

    wrote `' " ( [ {` autofill functions
    also `* _` for markdown files

--- lua/extra.lua
+++ lua/extra.lua
@@ -1,4 +1,4 @@
--- autocmds
+-- autocmds {{{
 au({ 'c', 'make', 'asm' }, "set noexpandtab" )
 au({ 'c', 'make', 'asm' }, "set tabstop=8" )
 au({ 'c', 'make', 'asm' }, "set softtabstop=8" )
@@ -11,3 +11,55 @@ au({ 'python', 'toml', 'yaml', 'nim', }, "set softtabstop=4" )
 au({ 'python', 'toml', 'yaml', 'nim', }, "set shiftwidth=4" )
 
 au({ 'plaintex', }, "set filetype=tex")
+
+au({ 'markdown', 'rmd' }, ':silent lua AutoFillMarkdown()')
+
+au({ 'c', 'cpp', 'python', 'lua', 'sh', 'make' },
+	':silent lua AutoFill()'
+)
+-- }}}
+
+-- global autofill function {{{
+Autofill = 0
+function AutoFill()
+	if Autofill == 0 then
+		Autofill = 1
+		map('i', '"', '""<Left>')
+		map("i", "'", "''<Left>")
+		map('i', '`', '``<Left>')
+		map('i', '(', '()<Left>')
+		map('i', '[', '[]<Left>')
+		map('i', '{', '{}<Left>')
+		print("[AutoFill() Enabled]")
+	else
+		Autofill = 0
+		umap('i', '"')
+		umap("i", "'")
+		umap('i', '`')
+		umap('i', '(')
+		umap('i', '[')
+		umap('i', '{')
+		print("[AutoFill() Disabled]")
+	end
+end
+-- }}}
+-- markdown autofill function {{{
+Mautofill = 0
+function AutoFillMarkdown()
+	if Mautofill == 0 then
+		Mautofill = 1
+		map('i', '*', '**<Left>')
+		map('i', '_', '__<Left>')
+		print('[md autofill Enabled]')
+	else
+		Mautofill = 0
+		umap('i', '*')
+		umap('i', '_')
+		print('[md autofill Disabled]')
+	end
+end
+-- }}}
+
+map('n', '<leader>af', ':lua AutoFill()<CR>')
+map('n', '<leader>am', ':lua AutoFillMarkdown()<CR>')
+--map('n', '<leader>ac', ':lua AsciiChars()<CR>')
