3209612d47071462c38813e687d86c5b662b25a9
hossein <hosteam01@gmail.com>
2022-12-02 00:41:47 +0330

    renamed
    added custom terminals

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,3 +1,6 @@
+-- helper {{{
+local cmd = vim.cmd
+-- }}}
 require("nvim-tree").setup({
 	open_on_setup = false,
 	auto_reload_on_write = true,
@@ -30,10 +33,25 @@ require("nvim-tree").setup({
 })
 
 vim.api.nvim_create_autocmd("BufEnter", {
-  nested = true,
-  callback = function()
-    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
-      vim.cmd "quit"
-    end
-  end
+	nested = true,
+	callback = function()
+		if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+			vim.cmd "quit"
+		end
+	end
 })
+
+-- NvimTree Toggle {{{
+NvimTreeOpen = 0
+
+function nvim_tree_toggle()
+	if NvimTreeOpen == 0 then
+		NvimTreeOpen = 1
+		cmd [[:NvimTreeOpen]]
+		cmd "call win_gotoid(1000)"
+	else
+		NvimTreeOpen = 0
+		cmd [[:NvimTreeClose]]
+	end
+end
+-- }}}
