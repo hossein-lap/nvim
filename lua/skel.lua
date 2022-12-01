cae76435c44ba5417ffa185eb5f8028aa8661689
hossein <hosteam01@gmail.com>
2022-12-01 23:05:02 +0330

    moved helper functions to the file
    defined a variable for the skel files' path

--- lua/skel.lua
+++ lua/skel.lua
@@ -1,12 +1,28 @@
-map('n', ',C', ':-1read ~/.config/nvim/template/skel.c<CR>G')
-map('n', ',cpp', ':-1read ~/.config/nvim/template/skel.cpp<CR>G')
-map('n', ',py', ':-1read ~/.config/nvim/template/skel.py<CR>G')
-map('n', ',lua', ':-1read ~/.config/nvim/template/skel.lua<CR>G')
-map('n', ',sh', ':-1read ~/.config/nvim/template/skel.sh<CR>G')
-map('n', ',mk', ':-1read ~/.config/nvim/template/skel.mk<CR>G')
-map('n', ',ms', ':-1read ~/.config/nvim/template/skel.ms<CR>G')
-map('n', ',tex', ':-1read ~/.config/nvim/template/skel.tex<CR>G')
-map('n', ',rmd', ':-1read ~/.config/nvim/template/skel.rmd<CR>G')
-map('n', ',html', ':-1read ~/.config/nvim/template/skel.html<CR>G')
+-- helper {{{
+local api = vim.api
+local skelpath = '~/.config/nvim/template/'
 
-map('n', ',eb', ':read ~/.config/nvim/template/eb.latex<CR>o')
+-- map() {{{
+local function map(mode, key, command, opts)
+	local options = { noremap = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	api.nvim_set_keymap(mode, key, command, options)
+end
+-- }}}
+-- }}}
+
+map('n',	',C',	':-1read ' .. skelpath .. 'skel.c<CR>G')
+map('n',	',cpp',	':-1read ' .. skelpath .. 'skel.cpp<CR>G')
+map('n',	',py',	':-1read ' .. skelpath .. 'skel.py<CR>G')
+map('n',	',lua',	':-1read ' .. skelpath .. 'skel.lua<CR>G')
+map('n',	',sh',	':-1read ' .. skelpath .. 'skel.sh<CR>G')
+map('n',	',mk',	':-1read ' .. skelpath .. 'skel.mk<CR>G')
+map('n',	',ms',	':-1read ' .. skelpath .. 'skel.ms<CR>G')
+map('n',	',tex',	':-1read ' .. skelpath .. 'skel.tex<CR>G')
+map('n',	',rmd',	':-1read ' .. skelpath .. 'skel.rmd<CR>G')
+map('n',	',md',	':-1read ' .. skelpath .. 'skel.md<CR>G')
+map('n',	',htm',	':-1read ' .. skelpath .. 'skel.html<CR>G')
+
+map('n',	',eb',	':read ' .. skelpath .. 'eb.latex<CR>o')
