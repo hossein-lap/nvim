42f6ec9326fff5af6e016b9b91b84efba1f341be
hossein <hosteam01@gmail.com>
2024-03-21 06:38:24 +0330

    fix telescope border; add description for key bindings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,24 +7,32 @@ return {
         "nvim-lua/plenary.nvim"
     },
 
-    config = function()
-        require('telescope').setup({})
+	config = function()
+
+		require('telescope').setup({
+			defaults = {
+				border = true,
+				borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└'},
+				winblend = 10,
+			},
+		})
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Find Files"})
+        vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Find Git Files"})
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
-        end)
+        end, {desc = "Grep string to quickfix list"})
         vim.keymap.set('n', '<leader>pWs', function()
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
         end)
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        end, {desc = "Search files that contains string"})
+        vim.keymap.set('n', '<leader>vh', builtin.help_tags, { desc = "Serach help pages"})
+
     end
 }
 
