f82b602116f21eb258bdc7bd365898b229ff2b28
hossein <hosteam01@gmail.com>
2024-04-10 03:24:00 +0330

    Add better icon support

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,11 @@
 return {
 	{
-		-- "nvim-tree/nvim-web-devicons"
+		"nvim-tree/nvim-web-devicons",
+		config = function()
+			local devicon = require'nvim-web-devicons'
+			devicon.setup {
+				color_icons = false,
+			}
+		end
 	},
 }
