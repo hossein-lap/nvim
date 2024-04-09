ab4c1ca8aed39ea5fbc1da757a2606bc122a7a6f
hossein <hosteam01@gmail.com>
2024-04-10 03:22:44 +0330

    Add rose-pine scheme

--- lua/plugins/schemes.lua
+++ lua/plugins/schemes.lua
@@ -14,14 +14,16 @@ return {
 			"hossein-lap/vim-lupper",
 			"hossein-lap/vim-octave",
 			"hossein-lap/vim-wal",
+			{ "rose-pine/neovim", name = "rose-pine" }
 		},
 
 		config = function()
 			local scheme = require("schemes")
 			scheme.ColorThem({
-				name = "hybrid",
+				-- name = "vim-wal",
+				name = "rose-pine",
 				alpha_enable = true,
-				alpha_sec = "cols", -- basic, cols, folds, all
+				alpha_sec = "basic", -- basic, cols, folds, all
 			})
 		end
 	},
